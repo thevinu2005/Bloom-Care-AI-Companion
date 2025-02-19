94b1fc2c9f31f21dc315ab32d56215f61250a1ed
thevinu2005 <thevinu2005@gmail.com>
2025-02-19 22:39:24 +0530

    setting up firebase

--- bloom_care/lib/main.dart
+++ bloom_care/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:bloom_care/screens/emotion_check/emotion_check.dart';
 import 'package:bloom_care/screens/emergancy/emergancy_page_1.dart';
 import 'package:bloom_care/screens/emergancy/emergancy_page_2.dart';
@@ -7,7 +9,11 @@ import 'package:bloom_care/screens/emergancy/emergancy_page_2.dart';
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
