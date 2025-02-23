fba97b4e70abdf7a871a1ff2e71a4c8d8fa19207
thevinu2005 <thevinu2005@gmail.com>
2025-02-23 22:54:08 +0530

    completed the code of service of API

--- bloom_care/lib/services/ml_service.dart
+++ bloom_care/lib/services/ml_service.dart
@@ -10,9 +10,24 @@ class ApiService {
       final request = http.MultipartRequest('POST', uri);
       
       
-    
+      final file = await http.MultipartFile.fromPath(
+        'audio',
+        filePath,
+        filename: 'audio_recording.wav',
+      );
       request.files.add(file);
 
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to upload audio: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error uploading audio: $e');
+    }
       
       
   }
