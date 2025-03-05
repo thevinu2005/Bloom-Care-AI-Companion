c1afab2ce4738ec6fcb83ad387e85e0312b9ecca
thevinu2005 <thevinu2005@gmail.com>
2025-03-05 13:47:40 +0530

    removed some dependancies

--- bloom_care/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bloom_care/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,7 @@ import facebook_auth_desktop
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_macos
-import geolocator_apple
 import google_sign_in_ios
-import maps_launcher
 import path_provider_foundation
 import record_darwin
 import url_launcher_macos
@@ -23,9 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  MapsLauncherPlugin.register(with: registry.registrar(forPlugin: "MapsLauncherPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
